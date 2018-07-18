918f2103b9f19feb5e3151769f3241fccb64c048
cpbeeker <chris.beekman.05@gmail.com>
2018-07-18 00:08:52 -0700

    more storyboard

---
+++ app/app/src/main/java/gloominati_selects/altair/OpenProject.java
@@ -0,0 +1,13 @@
+package gloominati_selects.altair;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OpenProject extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_project);
+    }
+}
